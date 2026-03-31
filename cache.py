acdb00f1a074666bd0b48dd874ec4e7f5e048181
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-04-01 01:49:52 +0530

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,15 +1,15 @@
-import aioredis
+import redis.asyncio as redis
 import json
 
-redis = None
+r = None
 
 async def init():
-    global redis
-    redis = await aioredis.from_url("redis://localhost", decode_responses=True)
+    global r
+    r = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
 async def get(key):
-    data = await redis.get(key)
+    data = await r.get(key)
     return json.loads(data) if data else None
 
 async def set(key, value):
-    await redis.set(key, json.dumps(value), ex=3600)
+    await r.set(key, json.dumps(value), ex=3600)
