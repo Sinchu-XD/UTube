6db5a75c25683e65d07b81c8aae32f92d1da916d
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-04-12 08:42:46 +0530

    JioSaavn API

--- Stream.py
+++ Stream.py
@@ -1,73 +1,38 @@
-import subprocess
 from fastapi.responses import StreamingResponse
+import aiohttp
+from JioSaavn import search
 
 
-def get_video_audio_urls(url: str, quality="auto"):
+async def get_stream_url(query: str):
+    results = await search(query, limit=1)
 
-    formats = {
-        "360": "bestvideo[height<=360][ext=mp4]+bestaudio[ext=m4a]",
-        "480": "bestvideo[height<=480][ext=mp4]+bestaudio[ext=m4a]",
-        "720": "bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]",
-        "1080": "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]",
-        "1440": "bestvideo[height<=1440][ext=mp4]+bestaudio[ext=m4a]",
-        "2160": "bestvideo[height<=2160][ext=mp4]+bestaudio[ext=m4a]"
-    }
-
-    if quality == "auto":
-        fmt = "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]"
-    else:
-        fmt = formats.get(quality, formats["720"])
-
-    cmd = [
-        "yt-dlp",
-        "--cookies", "cookies.txt",
-        "--js-runtimes", "node",
-        "--remote-components", "ejs:github",
-        "--extractor-args", "youtube:player_js_variant=main",
-        "-f", fmt,
-        "--no-playlist",
-        "-g",
-        url
-    ]
-
-    result = subprocess.run(
-        cmd,
-        capture_output=True,
-        text=True
-    )
-
-    if result.returncode != 0:
-        print(result.stderr)
+    if not results:
         return None, None
 
-    urls = result.stdout.strip().split("\n")
+    song = results[0]
 
-    if len(urls) < 2:
-        return None, None
-
-    return urls[0], urls[1]
+    return song.get("media_url"), song
 
 
-def stream_merged(video_url: str, audio_url: str):
+async def stream_audio(query: str):
+    stream_url, song = await get_stream_url(query)
 
-    ffmpeg_cmd = [
-        "ffmpeg",
-        "-i", video_url,
-        "-i", audio_url,
-        "-c:v", "copy",
-        "-c:a", "copy",
-        "-f", "mp4",
-        "-movflags", "frag_keyframe+empty_moov",
-        "pipe:1"
-    ]
+    if not stream_url:
+        return {"error": "Song not found"}
 
-    process = subprocess.Popen(
-        ffmpeg_cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.DEVNULL
-    )
+    session = aiohttp.ClientSession()
+
+    async def generator():
+        async with session.get(stream_url) as resp:
+            async for chunk in resp.content.iter_chunked(1024):
+                yield chunk
+        await session.close()
 
     return StreamingResponse(
-        process.stdout,
-        media_type="video/mp4"
+        generator(),
+        media_type="audio/mpeg",
+        headers={
+            "X-Title": song.get("song"),
+            "X-Artist": song.get("primary_artists")
+        }
     )
